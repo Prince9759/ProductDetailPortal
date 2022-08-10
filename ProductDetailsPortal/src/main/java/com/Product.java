7c51504a5ecb68e2d60f116726ae0bfb11755c63
Prince9759 <prince.jain_cs18@gla.ac.in>
2022-08-10 15:12:21 +0530

    Create Product.java

---
+++ ProductDetailsPortal/src/main/java/com/Product.java
@@ -0,0 +1,27 @@
+package com;
+
+public class Product 
+{
+	private int Pid;
+	private String Pname;
+	private int Price;
+	public int getPid() {
+		return Pid;
+	}
+	public void setPid(int pid) {
+		Pid = pid;
+	}
+	public String getPname() {
+		return Pname;
+	}
+	public void setPname(String pname) {
+		Pname = pname;
+	}
+	public int getPrice() {
+		return Price;
+	}
+	public void setPrice(int price) {
+		Price = price;
+	}
+
+}
